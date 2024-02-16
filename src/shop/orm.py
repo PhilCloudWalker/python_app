64b2414e41a7cce7355f40ed4ed7e2c1f0f593cb
Philipp.DAngelo.external <philipp_angelo@gmx.de>
2024-02-16 08:52:47 +0100

    Fix orm and add database creation

--- src/shop/orm.py
+++ src/shop/orm.py
@@ -1,9 +1,22 @@
-from sqlalchemy import Table, MetaData, Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import (
+    Table,
+    MetaData,
+    Column,
+    Integer,
+    String,
+    ForeignKey,
+    DateTime,
+    Date,
+    create_engine,
+)
 from sqlalchemy.orm import mapper, registry, relationship
 from shop.model import OrderLine, Batch
+from sqlalchemy_utils import database_exists, create_database
+import sys
 
 metadata = MetaData()
 mapper_registry = registry(metadata=metadata)
+DB_URL = "sqlite:///data.db"
 
 order_line = Table(
     "order_lines",
@@ -12,25 +25,39 @@ order_line = Table(
     Column("orderid", String(50)),
     Column("sku", String(50)),
     Column("qty", Integer),
-    Column("batch_id", String, ForeignKey("batches.reference"))
+    Column("batch_id", String, ForeignKey("batches.reference")),
 )
 
 batch = Table(
     "batches",
-    metadata, 
+    metadata,
     Column("reference", String, primary_key=True),
     Column("sku", String),
-    Column("eta", DateTime),
-    Column("_purchased_quantity", Integer)
+    Column("eta", Date),
+    Column("_purchased_quantity", Integer),
 )
 
 
 def start_mappers():
     line_mapper = mapper_registry.map_imperatively(OrderLine, order_line)
     mapper_registry.map_imperatively(
-        Batch, 
-        batch, 
-        properties ={
-            "_allocations": relationship(OrderLine, backref="order_lines", collection_class=set)
-        }
+        Batch,
+        batch,
+        properties={
+            "_allocations": relationship(
+                OrderLine, backref="order_lines", collection_class=set
+            )
+        },
     )
+
+
+def validate_database():
+    engine = create_engine(DB_URL, echo=True)
+    if not database_exists(engine.url):  # Checks for the first time
+        create_database(engine.url)  # Create new DB
+        metadata.create_all(engine)
+        print(
+            f"New Database Created: {database_exists(engine.url)}"
+        )  # Verifies if database is there or not.
+    else:
+        print("Database Already Exists")
--- tests/shop/test_orm.py
+++ tests/shop/test_orm.py
@@ -1,8 +1,5 @@
 import shop.model as model
-import pytest
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, sessionmaker, clear_mappers
-from shop.orm import start_mappers, metadata
+from datetime import date
 
 
 def test_orderline_mapper_can_save_lines(session):
@@ -27,3 +24,68 @@ def test_orderline_mapper_can_load_lines(session):
         model.OrderLine("order2", "BLUE-LIPSTICK", 14),
     ]
     assert session.query(model.OrderLine).all() == expected
+
+
+def test_batch_mapper_can_load_batches(session):
+    session.execute(
+        "INSERT INTO batches (reference, sku, eta, _purchased_quantity) VALUES "
+        '("ref1", "IN-STOCK", NULL, 100),'
+        '("ref2", "ON-THE-WAY", "2022-02-24", 50)'
+    )
+    expected = [
+        model.Batch("ref1", "IN-STOCK", 100, None),
+        model.Batch("ref2", "ON-THE-WAY", 50, date(2022, 2, 24)),
+    ]
+    assert session.query(model.Batch).all() == expected
+
+
+def test_saving_batches(session):
+    batch = model.Batch("batch1", "sku1", 100, eta=None)
+    session.add(batch)
+    session.commit()
+
+    rows = session.execute(
+        'SELECT reference, sku, _purchased_quantity, eta FROM "batches"'
+    )
+    assert list(rows) == [("batch1", "sku1", 100, None)]
+
+
+def test_saving_allocations(session):
+    batch = model.Batch("batch1", "sku1", 100, eta=None)
+    line = model.OrderLine("order1", "sku1", 10)
+
+    batch.allocate(line)
+    session.add(batch)
+    session.commit()
+
+    rows = list(session.execute('SELECT batch_id FROM "order_lines"'))
+    assert rows == [("batch1",)]
+
+
+def test_update_batch_with_allocations(session):
+    batch = model.Batch("batch1", "sku1", 100, eta=None)
+    session.add(batch)
+    session.commit()
+
+    line = model.OrderLine("order1", "sku1", 10)
+    batch.allocate(line)
+    session.add(batch)
+    session.commit()
+
+    rows = list(session.execute('SELECT batch_id FROM "order_lines"'))
+    assert rows == [("batch1",)]
+    batches = session.query(model.Batch).all()
+    assert len(batches) == 1
+
+
+def test_retrieving_allocations(session):
+    session.execute(
+        "INSERT INTO batches (reference, sku, _purchased_quantity, eta)"
+        ' VALUES ("batch1", "sku1", 100, null)'
+    )
+    session.execute(
+        'INSERT INTO order_lines (orderid, sku, qty, batch_id) VALUES ("order1", "sku1", 12, "batch1")'
+    )
+
+    batch = session.query(model.Batch).one()
+    assert batch._allocations == {model.OrderLine("order1", "sku1", 12)}
